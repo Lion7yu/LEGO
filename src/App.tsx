5b8995f5527290ba1b83d160220154ff8eb0a79d
Lionyu <779625444@qq.com>
2021-12-15 22:30:14 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import styled from "styled-components";
+import "./App.css";
+import Button from "./components/Button";
+
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/lego">LEGO</Link>
+            </li>
+            <li>
+              <Link to="/user">个人主页</Link>
+            </li>
+            <li>
+              <Link to="/export">导出页面</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/lego">
+            <Lego />
+          </Route>
+          <Route path="/user">
+            <User />
+          </Route>
+          <Route path="/export">
+            <Export />
+          </Route>
+          <Redirect exact from="/" to="/lego" />
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Export() {
+  return <h2>Export</h2>;
+}
+
+function Lego() {
+  return <h2>LEGO</h2>;
+}
+
+function User() {
+  return <h2>User</h2>;
+}
+
+function NoMatch() {
+  return <h1>404 Not found</h1>;
 }
 
 export default App;
